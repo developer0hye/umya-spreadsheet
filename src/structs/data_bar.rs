201e321f2e5c209cc92cce568d7e53ded559d85d
Yonghye Kwon <developer.0hye@gmail.com>
2026-07-18 12:51:35 +0900

    fix: read and write the dataBar minLength, maxLength and showValue attributes
    The dataBar element's bar-scaling attributes were dropped on read and
    never serialized, so consumers could not honor Excel's bar geometry
    (spec defaults: minLength 10, maxLength 90, showValue true). Parse
    them, expose accessors that fall back to the spec defaults, and write
    them back when explicitly set.
    
    Signed-off-by: Yonghye Kwon <developer.0hye@gmail.com>

--- src/structs/data_bar.rs
+++ src/structs/data_bar.rs
@@ -1,5 +1,7 @@
+use super::BooleanValue;
 use super::Color;
 use super::ConditionalFormatValueObject;
+use super::UInt32Value;
 use crate::reader::driver::*;
 use crate::writer::driver::*;
 use quick_xml::events::BytesStart;
@@ -11,11 +13,62 @@ use thin_vec::ThinVec;
 
 #[derive(Clone, Default, Debug)]
 pub struct DataBar {
+    min_length: UInt32Value,
+    max_length: UInt32Value,
+    show_value: BooleanValue,
     cfvo_collection: ThinVec<ConditionalFormatValueObject>,
     color_collection: ThinVec<Color>,
 }
 
 impl DataBar {
+    /// minLength attribute; the OOXML default is 10 (percent of cell width).
+    #[inline]
+    pub fn get_min_length(&self) -> u32 {
+        if self.min_length.has_value() {
+            *self.min_length.get_value()
+        } else {
+            10
+        }
+    }
+
+    #[inline]
+    pub fn set_min_length(&mut self, value: u32) -> &mut Self {
+        self.min_length.set_value(value);
+        self
+    }
+
+    /// maxLength attribute; the OOXML default is 90 (percent of cell width).
+    #[inline]
+    pub fn get_max_length(&self) -> u32 {
+        if self.max_length.has_value() {
+            *self.max_length.get_value()
+        } else {
+            90
+        }
+    }
+
+    #[inline]
+    pub fn set_max_length(&mut self, value: u32) -> &mut Self {
+        self.max_length.set_value(value);
+        self
+    }
+
+    /// showValue attribute; the OOXML default is true.
+    #[inline]
+    pub fn get_show_value(&self) -> bool {
+        if self.show_value.has_value() {
+            *self.show_value.get_value()
+        } else {
+            true
+        }
+    }
+
+    #[inline]
+    pub fn set_show_value(&mut self, value: bool) -> &mut Self {
+        self.show_value.set_value(value);
+        self
+    }
+
     #[inline]
     pub fn get_cfvo_collection(&self) -> &[ConditionalFormatValueObject] {
         &self.cfvo_collection
@@ -56,8 +109,12 @@ impl DataBar {
     pub(crate) fn set_attributes<R: std::io::BufRead>(
         &mut self,
         reader: &mut Reader<R>,
-        _e: &BytesStart,
+        e: &BytesStart,
     ) {
+        set_string_from_xml!(self, e, min_length, "minLength");
+        set_string_from_xml!(self, e, max_length, "maxLength");
+        set_string_from_xml!(self, e, show_value, "showValue");
+
         xml_read_loop!(
             reader,
             Event::Empty(ref e) => {
@@ -86,7 +143,20 @@ impl DataBar {
 
     pub(crate) fn write_to(&self, writer: &mut Writer<Cursor<Vec<u8>>>) {
         // dataBar
-        write_start_tag(writer, "dataBar", vec![], false);
+        let mut attributes: Vec<(&str, &str)> = Vec::new();
+        let min_length = self.min_length.get_value_string();
+        if self.min_length.has_value() {
+            attributes.push(("minLength", &min_length));
+        }
+        let max_length = self.max_length.get_value_string();
+        if self.max_length.has_value() {
+            attributes.push(("maxLength", &max_length));
+        }
+        let show_value = self.show_value.get_value_string();
+        if self.show_value.has_value() {
+            attributes.push(("showValue", show_value));
+        }
+        write_start_tag(writer, "dataBar", attributes, false);
 
         // cfvo
         for v in &self.cfvo_collection {
